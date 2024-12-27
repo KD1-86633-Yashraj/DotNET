f94c5ff66178c3f6a7463fdcfbb3d76f0e0d5142
YASHRAJ SHITOLE <yashrajs927@gmail.com>
2024-12-27 17:29:11 +0530

    Assignment 02

---
+++ Assignment02/Assignment02_Question01/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+//Write a structure Student with following members:
+// string name; bool gender; int age; ----------------------------------DONE
+//int std; char div; double marks; -------------------------------------DONE
+//Provide following functionality:
+//Default constructor(?) -----------------------------------------------DONE
+//Parameterized constructor --------------------------------------------DONE
+//Get and Set methods --------------------------------------------------DONE
+//AcceptDetails() method to accept data from console. ------------------DONE
+//PrintDetails() method to print data to console. ----------------------DONE
+//Check the default access specifier for members of structure. ---------DONE
+//Keep all data members private and all member functions public -------DONE
+
+namespace Assignment02_Question01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student();
+
+            student.acceptData();
+            student.displayData();
+
+        }
+    }
+
+        public struct Student
+        {
+
+        private string _Name;
+        private bool _Gender;
+        private int _Age;
+        private int _Std;
+        private char _Div;
+        private double _Marks;
+
+
+
+        public Student(string name, bool gender, int age, int std, char div, double marks)
+        {
+            _Name = name;
+            _Gender = gender;
+            _Age = age;
+            _Std = std;
+            _Div = div;
+            _Marks = marks;
+        }
+
+
+
+
+
+        public void acceptData()
+        {
+            Console.WriteLine("Enter name");
+            _Name = Console.ReadLine();
+
+            bool flag = true;
+            while (flag)
+            {
+
+            Console.WriteLine("Enter 1.MALE 2.FEMALE");
+                switch (int.Parse(Console.ReadLine())) {
+                    case 1:
+                        {
+                            _Gender = true;
+                            flag = false;
+                            break;
+                        }
+
+                    case 2: { 
+                            _Gender= false;
+                            flag = false;
+                            break;
+                        }
+                    default: {
+                            Console.WriteLine("Wrong choice");
+                            break;
+                        }
+                }
+            }
+
+
+            Console.WriteLine("Engter Age");
+            _Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter std");
+            _Std=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter div");
+            _Div = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter mark");
+            _Marks=int.Parse(Console.ReadLine());
+
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("Name: "+_Name);
+
+            if (_Gender == true)
+            {
+                Console.WriteLine("Gender: Male");
+            }
+            else
+            {
+                Console.WriteLine("Gender: Female");
+            }
+
+            Console.WriteLine("Age: "+_Age);
+            Console.WriteLine("Std: "+_Std);
+            Console.WriteLine("Div: "+_Div);
+            Console.WriteLine("Marks: "+_Marks);
+        }
+
+
+
+
+
+
+        public double Marks
+        {
+            get { return _Marks; }
+            set { _Marks = value; }
+        }
+
+
+        public char Div
+        {
+            get { return _Div; }
+            set { _Div = value; }
+        }
+
+
+        public int Std
+        {
+            get { return _Std; }
+            set { _Std = value; }
+        }
+
+
+        public int Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+
+
+        public bool Gender
+        {
+            get { return _Gender; }
+            set { _Gender = value; }
+        }
+
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Assignment02/Assignment02_Question02/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Create a reference of Student’s array in Main. Write a function CreateArray() to
+//allocate the array as per user’s requirement. Pass it to a function AcceptInfo() to
+//accept student information. Print the array using PrintInfo() function.Also write a
+//function ReverseArray() that reverse the array into a new array.Reprint the modified
+//array in Main.
+//Note: Do not return array from the function. All four functions mentioned above
+//should be static functions in Main()’s class. 
+
+namespace Assignment02_Question02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student();
+
+            Console.WriteLine("Enter size of array");
+            Student[] studentsArray = CreateArray(int.Parse(Console.ReadLine()));
+
+            PrintArray(studentsArray);
+
+            Console.WriteLine("Reversed array");
+            PrintArray(ReverseArray(studentsArray));
+
+
+
+
+
+            
+        }
+
+        public static Student[] CreateArray(int count) { 
+
+            Student[] array = new Student[count];
+
+            for (int i = 0; i < count; i++) {
+
+                array[i] = new Student();
+
+                array[i].AcceptData();
+            }
+
+            return array;
+        }
+
+        public static void PrintArray(Student[] array) {
+            foreach (Student i in array) {
+
+                i.DisplayData();
+
+            }
+        }
+        
+        public static Student[] ReverseArray(Student[] arr)
+        {
+
+            Student[] reversedArray=new Student[arr.Length];
+
+            for (int i = 0 ,j=arr.Length-1; i < reversedArray.Length && j>=0; i++, j--) {
+
+                    reversedArray[i] = arr[j];
+                
+            }
+            return reversedArray;
+
+        }
+
+    }
+}
